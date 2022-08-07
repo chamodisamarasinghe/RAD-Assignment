f58c5eba79acdbb5a116da2be2a17c49f96c47f8
chamodisamarasinghe <chamodisandunika98@gmail.com>
2022-08-07 15:55:59 +0530

    Updated file

--- src/app/App.js
+++ src/app/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import {Routes, Route, BrowserRouter} from "react-router-dom";
 import '../App.css';
 import Login from "../pages/sessions/Login";
+import Register from "../pages/sessions/UserRegistration";
 
 function App() {
   return (
       <BrowserRouter>
         <Routes>
             <Route exact path='/' element={<Login/>}/>
+            <Route exact path='register' element={<Register/>}/>
         </Routes>
       </BrowserRouter>
   );

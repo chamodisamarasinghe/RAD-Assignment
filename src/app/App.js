9da0276bb582c13e2501e60bd9978eecc68163a0
chamodisamarasinghe <chamodisandunika98@gmail.com>
2022-08-08 01:44:33 +0530

    Updated file

--- src/app/App.js
+++ src/app/App.js
@@ -3,6 +3,10 @@ import {Routes, Route, BrowserRouter} from "react-router-dom";
 import '../App.css';
 import Login from "../pages/sessions/Login";
 import Register from "../pages/sessions/UserRegistration";
+import DashBoard from "../pages/sessions/Dash";
+import Product from "../pages/sessions/Product";
+
+
 
 function App() {
   return (
@@ -10,6 +14,8 @@ function App() {
         <Routes>
             <Route exact path='/' element={<Login/>}/>
             <Route exact path='register' element={<Register/>}/>
+            <Route exact path='dash' element={<DashBoard/>}/>
+            <Route exact path='product' element={<Product/>}/>
         </Routes>
       </BrowserRouter>
   );

f926eaa9c67bbbba27ec5d8d69f4795f1d071073
chamodisamarasinghe <chamodisandunika98@gmail.com>
2022-08-08 01:52:03 +0530

    Added file

---
+++ src/services/ProductService.js
@@ -0,0 +1,70 @@
+import axios from "axios";
+
+class ProductService {
+
+    baseURL = "http://localhost:8080/backEnd/api/v1/";
+
+    addDriver = async (data) => {
+        const promise = new Promise((resolve, reject) => {
+            axios.post(`${this.baseURL}admin/driver`, data)
+                .then((res) => {
+                    return resolve(res)
+                })
+                .catch((er) => {
+                    return resolve(er)
+                })
+        })
+        return await promise
+    }
+
+
+    putDriver = async (data) => {
+        const promise = new Promise((resolve, reject) => {
+            axios.put(`${this.baseURL}admin/driver`, data)
+                .then((res) => {
+
+                    return resolve(res)
+                })
+                .catch((er) => {
+                    return resolve(er)
+                })
+        })
+        return await promise;
+    };
+
+
+
+    deleteDriver = async (params) => {
+        const promise = new Promise((resolve, reject) => {
+            axios.delete(`${this.baseURL}admin/driver`, {params: params})
+                .then((res) => {
+                    return resolve(res)
+                })
+                .catch((err) => {
+                    return resolve(err)
+                })
+        })
+        return await promise;
+    };
+
+
+
+    fetchDriver = async () => {
+        const promise = new Promise((resolve, reject) => {
+            axios.get(`${this.baseURL}admin/driver`)
+                .then((res) => {
+                    return resolve(res)
+                })
+                .catch((err) => {
+                    return resolve(err)
+                })
+        })
+        return await promise;
+    }
+
+
+
+
+}
+
+export default new ProductService()

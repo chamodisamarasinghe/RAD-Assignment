62cdcdb6cd801783dca98930d754ca97627caa3e
chamodisamarasinghe <chamodisandunika98@gmail.com>
2022-08-09 01:08:14 +0530

    Added file

---
+++ src/pages/sessions/Dash/nav.jsx
@@ -0,0 +1,42 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import {Link} from "@mui/material";
+import Button from "@mui/material/Button";
+import Stack from '@mui/material/Stack';
+
+export default function DashBoardNav() {
+    return (
+        <Box sx={{flexGrow: 1,color:"green"}}>
+            <AppBar position="static">
+                <Toolbar variant="dense">
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                        {/*Easy Car Rental*/}
+                    </Typography>
+
+
+                    <Link href="dash" underline="none">
+                        <Button  color='primary' variant="contained" >Dash Board</Button>
+                    </Link>
+
+
+
+
+                    <Link href="product" underline="none">
+                        <Button  color='primary' variant="contained" sx={{ml:2}}>Product</Button>
+                    </Link>
+
+
+                    <Link href="viewSchedule" underline="none">
+                        <Button  color='primary' variant="contained" sx={{ml:4}}>Cart</Button>
+                    </Link>
+
+                </Toolbar>
+            </AppBar>
+        </Box>
+    );
+}

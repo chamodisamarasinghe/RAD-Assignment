77290c5660f8cd926ce225bc8b38d8844272fe5a
chamodisamarasinghe <chamodisandunika98@gmail.com>
2022-08-09 20:03:53 +0530

    Updated file

--- src/services/ProductService.js
+++ src/services/ProductService.js
@@ -1,70 +1,42 @@
-import axios from "axios";
+import axios from "../axios";
 
 class ProductService {
-
-    baseURL = "http://localhost:8080/backEnd/api/v1/";
-
-    addProduct = async (data) => {
+    fetchProducts = async () => {
         const promise = new Promise((resolve, reject) => {
-            axios.post(`${this.baseURL}admin/driver`, data)
+            axios.get('products')
                 .then((res) => {
-                    return resolve(res)
-                })
-                .catch((er) => {
-                    return resolve(er)
-                })
-        })
-        return await promise
-    }
-
-
-    putProduct = async (data) => {
-        const promise = new Promise((resolve, reject) => {
-            axios.put(`${this.baseURL}admin/driver`, data)
-                .then((res) => {
-
-                    return resolve(res)
-                })
-                .catch((er) => {
-                    return resolve(er)
-                })
-        })
-        return await promise;
-    };
-
-
-
-    deleteProduct = async (params) => {
-        const promise = new Promise((resolve, reject) => {
-            axios.delete(`${this.baseURL}admin/driver`, {params: params})
-                .then((res) => {
-                    return resolve(res)
+                    return resolve(res);
                 })
                 .catch((err) => {
-                    return resolve(err)
+                    return resolve(err);
                 })
-        })
-        return await promise;
-    };
-
-
-
-    fetchProduct = async () => {
-        const promise = new Promise((resolve, reject) => {
-            axios.get(`${this.baseURL}admin/driver`)
-                .then((res) => {
-                    return resolve(res)
-                })
-                .catch((err) => {
-                    return resolve(err)
-                })
-        })
+        });
         return await promise;
     }
-
-
-
-
+    fetchProductCategories = async () => {
+        const promise = new Promise((resolve, reject) => {
+            axios.get('products/categories')
+                .then((res) => {
+                    return resolve(res);
+                })
+                .catch((err) => {
+                    return resolve(err);
+                })
+        });
+        return await  promise;
+    }
+    postProduct = async (data) => {
+        const promise = new Promise((resolve, reject) => {
+            axios.post('products', data)
+                .then((res) => {
+                    return resolve(res);
+                })
+                .catch((res) => {
+                    return resolve(res);
+                })
+        });
+        return await promise;
+    }
 }
 
-export default new ProductService()
+export default new ProductService();

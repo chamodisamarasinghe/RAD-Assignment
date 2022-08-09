8c0b46c594260256e40bc57f28fadedd2bd5456e
chamodisamarasinghe <chamodisandunika98@gmail.com>
2022-08-09 20:06:57 +0530

    Updated file

--- src/services/ProductService.js
+++ src/services/ProductService.js
@@ -1,17 +1,7 @@
 import axios from "../axios";
 
 class ProductService {
-    fetchProducts = async () => {
-        const promise = new Promise((resolve, reject) => {
-            axios.get('products')
-                .then((res) => {
-                    return resolve(res);
-                })
-                .catch((err) => {
-                    return resolve(err);
-                })
-        });
-        return await promise;
+
     }
     fetchProductCategories = async () => {
         const promise = new Promise((resolve, reject) => {
